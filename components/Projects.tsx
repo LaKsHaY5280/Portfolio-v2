a617db0e3d36cbc2e069e1408f128aacdb2e0aac
Lakshay goyal <akshaybuster5280@gmail.com>
2023-11-12 20:24:08 +0530

    projects section done

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { motion } from "framer-motion";
+import ProjCard from "./shared/ProjCard";
 
 const Projects = () => {
   return (
     <motion.div
-      className=" relative flex justify-evenly items-center max-md:flex-col max-md:text-center max-w-full mx-auto md:px-10 pt-20 overflow-hidden h-screen"
+      className=" relative flex justify-evenly items-center max-md:flex-col max-md:text-center max-w-full mx-auto overflow-hidden z-0 h-screen"
       initial={{
         opacity: 0,
       }}
@@ -16,10 +17,16 @@ const Projects = () => {
         duration: 3,
       }}
     >
-      <h3 className=" absolute top-24 tracking-[20px] text-gray-500 text-2xl uppercase">
+      <h3 className=" absolute top-24 tracking-[20px] text-gray-500 text-2xl uppercase ">
         Projects
       </h3>
-      <div></div>
+      <div className=" relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
+        <ProjCard />
+        <ProjCard />
+        <ProjCard />
+        <ProjCard />
+      </div>
+      <div className=" w-full absolute top-[30%] bg-secondary/10 left-0 h-[300px] -skew-y-12" />
     </motion.div>
   );
 };
---
+++ components/shared/ProjCard.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { motion } from "framer-motion";
+import gericht from "@/assets/ssgericht.png";
+import Image from "next/image";
+import Link from "next/link";
+import { Github, ExternalLink } from "lucide-react";
+
+const ProjCard = () => {
+  return (
+    <div className=" flex justify-center items-center flex-col space-y-5 p-20 md:p-44 h-screen w-screen flex-shrink-0 snap-center">
+      <motion.div
+        initial={{
+          opacity: 0,
+          y: -300,
+        }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          duration: 1,
+        }}
+        viewport={{
+          once: true,
+        }}
+      >
+        <Image src={gericht} alt="gericht" className=" mt-14 max-w-md" />
+      </motion.div>
+      <div className=" space-y-4 md:space-y-10 px-0 md:px-10 max-w-6xl">
+        <h4 className=" text-xl md:text-4xl font-semibold text-center">
+          <span className=" animated_underline">#1 Gericht</span>
+        </h4>
+        <p className=" max-md:text-xs">
+          "Gericht" is a frontend-only web application that serves as a visual
+          platform for exploring different recipes. Built with React.js, as
+          evident from the GitHub repository, the application presents a clean
+          and user-friendly interface where users can browse, search, and view
+          details of various recipes. The website's live version showcases a
+          range of dishes with their respective images, ingredients, and cooking
+          instructions, providing an intuitive and engaging user experience for
+          food enthusiasts.
+        </p>
+        <ul className=" flex justify-around items-center">
+          <li>
+            <Link href="https://github.com/LaKsHaY5280/gericht" target="blank">
+              <Github color="#F7Ab0A" absoluteStrokeWidth />
+            </Link>
+          </li>
+          <li>
+            <Link href="https://gerichtlg.vercel.app/" target="blank">
+              <ExternalLink color="#F7Ab0A" absoluteStrokeWidth />
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default ProjCard;
